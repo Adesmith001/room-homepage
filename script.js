26121c4929df162ef3b39ae82aabbce5218464e7
Toluwani Somade <tsomade.2200314@stu.cu.edu.ng>
2025-06-26 11:08:26 +0100

    hamburger can now show the nav bar

--- script.js
+++ script.js
@@ -19,38 +19,74 @@ window.onload = function(){
 }
 
 function shiftImageLeft(){
-    var prevImg = currImg;
+    const prevImg = currImg;
     currImg = currImg - 1;
     if (currImg == 0){
         currImg = 3;
     }
 
-    var retrievePrevImg = document.getElementById(currFeatureImage[prevImg-1]);
-    var retrieveCurrImg = document.getElementById(currFeatureImage[currImg-1]);
+    const retrievePrevImg = document.getElementById(currFeatureImage[prevImg-1]);
+    const retrieveCurrImg = document.getElementById(currFeatureImage[currImg-1]);
 
     retrievePrevImg.className = "display-none";
     retrieveCurrImg.className = "";
 
-    var retrievePrevText = document.getElementById(currFeature[prevImg-1]);
-    var retrieveCurrText = document.getElementById(currFeature[currImg-1]);
+    const retrievePrevText = document.getElementById(currFeature[prevImg-1]);
+    const retrieveCurrText = document.getElementById(currFeature[currImg-1]);
 
     retrievePrevText.className = "display-none";
     retrieveCurrText.className = "current-box";
 }
 
 function shiftImageRight(){
-    var prevImg = currImg;
+    const prevImg = currImg;
     currImg = currImg + 1;
     if (currImg == 4){
         currImg = 1;
     }
 
-    var retrievePrevImg = document.getElementById(currFeatureImage[prevImg-1]);
-    var retrieveCurrImg = document.getElementById(currFeatureImage[currImg-1]);
+    const retrievePrevImg = document.getElementById(currFeatureImage[prevImg-1]);
+    const retrieveCurrImg = document.getElementById(currFeatureImage[currImg-1]);
     retrievePrevImg.className = "display-none";
     retrieveCurrImg.className = "";
-    var retrievePrevText = document.getElementById(currFeature [prevImg-1]);
-    var retrieveCurrText = document.getElementById(currFeature [currImg-1]);
+    const retrievePrevText = document.getElementById(currFeature [prevImg-1]);
+    const retrieveCurrText = document.getElementById(currFeature [currImg-1]);
     retrievePrevText.className = "display-none";
     retrieveCurrText.className = "current-box";
+}
+
+function adjustMenu(){
+    const siteLogo = document.getElementById("site-logo");
+    const navBar = document.getElementById("navbarControl");
+    const mobileMenuButton = document.getElementById("mobile-menu-button");
+    const closeMobileMenuButton = document.getElementById("mobile-close-menu-button");
+    const navOptions = document.getElementById("nav-options");
+
+    if (mobileMenuStatus == 0){
+        siteLogo.style.display = "none";
+        mobileMenuButton.style.display = "none";
+        closeMobileMenuButton.style.display = "inline";
+        navOptions.style.display = "flex";
+        navOptions.style.color = "black";
+        navOptions.style.position = "absolute";
+        navOptions.style.right = "30px";
+        navBar.style.backgroundColor = "white";
+        navBar.style.height = "120px";
+        navBar.style.top = "0px";
+        
+        mobileMenuStatus = 1;
+    }
+    else{
+        siteLogo.style.display = "inline";
+        mobileMenuButton.style.display = "inline";
+        closeMobileMenuButton.style.display = "none";
+        navOptions.style.color = "white";
+        navOptions.style.display = "none";
+        navOptions.style.position = "relative";
+        navBar.style.backgroundColor = "transparent";
+        navBar.style.height = "auto";
+        navBar.style.top = "45px";
+
+        mobileMenuStatus = 0;
+    }
 }
